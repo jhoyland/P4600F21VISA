7bcdd8e17fdcc2568472e031d2cf5b5c37e6dbaa
Erinn <erinn.psajd@gmail.com>
2021-09-21 11:46:42 -0700

    Updated end of class work: skeleton functions added to dataprocessing

--- src/dataprocessing.c
+++ src/dataprocessing.c
@@ -9,3 +9,69 @@
 */
 
 // Mean function: 
+float mean(float num[], int n)
+{
+	float mean = 0, sum=0;
+	int i = 0;
+
+	for (i=0;i<n;i++)
+	{
+		sum += num[i];
+	}
+
+	mean = sum/10;
+
+	return mean;
+}
+
+// Standard Deviation:
+float stdev(float num[], float mean, int n)
+{
+	float dif[n], x, m;
+	int i=0;
+	while (i<n)
+	{
+		x = num[i] - mean;
+		x = x*x;
+		dif[i] = x;
+		i++;
+	}
+
+	// mean of squared difs
+	i = 0;
+	float sum = 0;
+
+	for (i=0;i<n;i++)
+	{
+		sum += num[i];
+	}
+
+	m = sum/n;
+	// take the square root
+	double std; 
+	std = sqrt(m);
+
+	return std; 
+}
+
+
+// RMS Value: 
+
+float RMS(float num[])
+{
+
+}
+
+//Amplitude:
+
+float Amp(float num[])
+{
+
+}
+
+//Moving Average Filter:
+
+float MAF(float num[])
+{
+
+}

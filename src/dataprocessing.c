227d8ff51ea33463067bafc0a8a6b1b4d6cf3b5c
Erinn <erinn.psajd@gmail.com>
2021-09-21 11:08:33 -0700

    First Commit

---
+++ src/dataprocessing.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+/*
+  TASK:
+  Create, in a separate file a set of functions to calculate:
+  The mean, RMS value and amplitude and smoothed version of the random data generated
+*/
+
+// Mean function: 

1c4d8faf22628064a68a65725e8db0a789dce179
Dcamparmo <david.camparmo@emai.kpu.ca>
2021-09-21 11:49:59 -0700

    End of Day Sept, 21

--- src/camparmo.c
+++ src/camparmo.c
@@ -1,1 +1,21 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+double polynomial(double x, int p, double* c)
+{
+	double y = 0;
+	int i;
+
+	for(i=0;i<=p;i++)
+	{
+		y +=  c[i] * pow(x,i);
+	}
+
+	return y;
+}
+
+int main(int argc, char**argv)
+{
+
+}

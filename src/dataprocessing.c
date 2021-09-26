051523f4dfc05d3bcbc0843f4eb74f76fce870b1
James Hoyland <hoyland.jd@gmail.com>
2021-09-25 22:35:48 -0700

    Good work Erinn, see a couple of comments in your smoothing function

--- src/dataprocessing.c
+++ src/dataprocessing.c
@@ -9,7 +9,7 @@
   The mean, RMS value and amplitude and smoothed version of the random data generated
 */
 
-// Mean function: 
+// Mean function:
 float mean(double num[], int n)
 {
 	double mean = 0, sum=0;
@@ -33,6 +33,7 @@ double stdev(double num[], double mean, int n)
 	while (i<n)
 	{
 		x = num[i] - mean;
+		// JAMES: Probably quicker here just to say dif[i] = x*x;
 		x = x*x;
 		dif[i] = x;
 		i++;
@@ -49,23 +50,26 @@ double stdev(double num[], double mean, int n)
 
 	m = sum/n;
 	// take the square root
-	double std; 
+	double std;
 	std = sqrt(m);
 
-	return std; 
+	return std;
 }
 
+// JAMES: Take a look at your standard deviation and rms functions
+// They essentially do the same thing with one minor difference
+// Could they be combined into a single function?
 
-// RMS Value: 
+// RMS Value:
 
 double rms(double num[], int n)
 {
 	// n is the length of the array
 	double sum = 0, rms = 0;
-	for(int i=0; i<n; i++) //get the sum of squared values in array 
+	for(int i=0; i<n; i++) //get the sum of squared values in array
 	{
 		sum += pow(num[n],2);
-	}	
+	}
 	rms = sqrt(sum/n);
 
 	return rms;
@@ -75,9 +79,9 @@ double rms(double num[], int n)
 
 double Amp(double num[], int n)
 {
-	// n is the length of the array 
+	// n is the length of the array
 	// the following peak method is not averaged and might produce
-	//slight errors so it has been discarded 
+	//slight errors so it has been discarded
 	/*double peak[n], amp;
 	for (i=0; i<n; i++)
 	{
@@ -90,6 +94,8 @@ double Amp(double num[], int n)
 	amp = peak[0];
 	return amp; */
 	double amp;
+	// JAMES: math.h also has a constant M_SQRT2 which can be used instead
+	// of calling sqrt(2)
 	amp = rms(num, n) * sqrt(2);
 	return amp;
 }
@@ -99,17 +105,28 @@ double Amp(double num[], int n)
 float smooth(double *d, int n, int w, double* end)
 {
 	// where n is the length of the array, w is the window
-	// double* is empty array 
+	// double* is empty array
 	// moving average filter is the sum w # of elements/# of all elements
 	double filter = 0;
 
 	for(int i = 0; i < (n-w); i++) // cycle through without breaking upper limit
 	{
-		for(int j=0; j < w; j++) 
+		for(int j=0; j < w; j++)
 		{
+			// JAMES: This will cause problems, (1/w) will be calculated as
+			// integer division producing zero if w>1. The simplest fix is
+			// to write (1.0/w). But multiplying by 1/w is the same as dividing
+			// by w so :
+			// filter += d[i+j]/w;
 			filter += (1/w) * d[i+j];
+			// JAMES: However, this is not the most efficient approach,
+			// you are dividing each of the added terms by w, why not
+			// just divide the final sum of the window by w? This would
+			// save w-1 divisions per data point.
 		}
-		end[i] = d[i] * filter; 
+		// JAMES: You do not need to multiply by d[i] the contents of filter
+		// is the averaged value at that point.
+		end[i] = d[i] * filter;
 		filter = 0;
 	}
 }

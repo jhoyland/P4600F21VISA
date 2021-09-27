3360b073ee063bf30d119a7403543cf399157981
James Hoyland <hoyland.jd@gmail.com>
2021-09-27 15:11:44 -0700

    Setting up VISA comms for Tektronix scope

--- Makefile
+++ Makefile
@@ -1,23 +1,25 @@
+TARGET    = scopetest
+HOMEDIR   = /c/msys64/home/hoyla/P4600F21VISA
+
 # Compiler and linker options
 CC        = gcc
-CFLAGS    = -c -v -g
+CFLAGS    = -c -Wall -g
 LDFLAGS   =
 
 # User directories to search
-BUILDDIR  = ./build
-INCDIR    = ./include
-SRCDIR    = ./src
+BUILDDIR  = $(HOMEDIR)/build
+INCDIR    = $(HOMEDIR)/include
+SRCDIR    = $(HOMEDIR)/src
 
-HOMEDIR   = /c/msys64/home/hoyla/week3lib
 
 # 3rd Party library directories to search
-LIBDIR    = /mingw64/lib $(HOMEDIR)/libs
+LIBDIR    = /mingw64/lib $(HOMEDIR)/lib
 LIBINCDIR = $(HOMEDIR)/includes
 
 # List sources, object files and libraries to be used
 SOURCES   = $(wildcard $(SRCDIR)/*.c )
 OBJECTS   = $(patsubst $(SRCDIR)/%.c, $(BUILDDIR)/%.o, $(SOURCES))
-LIBRARIES =
+LIBRARIES = visa64
 
 # Create command line arguments
 LIBCMD    = $(addprefix -l,$(LIBRARIES))
@@ -25,7 +27,6 @@ LIBDIRCMD = $(addprefix -L,$(LIBDIR))
 INCCMD    = $(addprefix -I,$(INCDIR) $(LIBINCDIR))
 
 # The final executable name
-TARGET    = datamanip
 
 # The rules
 
--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <randdata.h>
 #include <time.h>
+#include "visa.h"
+#include <windows.h>
 
 /*
   TASK:
@@ -18,7 +20,60 @@ int main(int argc, char** argv)
   int ndata = 1024;
   double x[ndata];
 
-  random_data(ndata,x);
+  ViStatus status = VI_SUCCESS;
+  ViFindList resourceList;
+  ViUInt32 num_inst;
+  ViUInt32 resultCount;
+
+  ViSession defaultRM, scopeHandle;
+  ViChar description[VI_FIND_BUFLEN];
+  setvbuf(stdout,NULL,_IONBF,16);
+
+  //random_data(ndata,x);
+
+  status = viOpenDefaultRM(&defaultRM); //open the resource manager
+  if (status != VI_SUCCESS)
+  {
+    // Do something if RM cannot open
+  }
+  else
+  {
+    status = viFindRsrc(defaultRM,"USB[0-9]::0x0699?*INSTR",&resourceList,&num_inst,description); //find the scope
+		if (status != VI_SUCCESS)
+		{
+			// Do something if cannot find scope
+		}
+		else
+		{
+      printf("\nFound Scope");
+
+      status = viOpen(defaultRM,description,VI_NULL,VI_NULL,&scopeHandle); //open the scope and give it a handle
+      if(status != VI_SUCCESS)
+      {
+        // Do something if cannot open scope
+      }
+      else
+      {
+
+        printf("\nOpened Scope");
+
+        char ret[50];
+
+        viWrite(scopeHandle,"*IDN?\n",5,&resultCount);
+        //Sleep(1000);
+        viRead(scopeHandle,ret,50,&resultCount);
+
+
+        printf(ret);
+        printf("\nGot here");
+
+        //fflush(stdout);
+      }
+
+    }
+  }
+
+
 
   /*
     Code to test your functions goes here

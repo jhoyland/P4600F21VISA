3abcaf0ad4c42e2b5134a5eee546ab0450991b00
stonehenge51 <stonehenge515@gmail.com>
2021-09-28 11:54:49 -0700

    created first Visa protocal commands to control Oscilloscope

--- Makefile
+++ Makefile
@@ -11,13 +11,13 @@ SRCDIR    = ./src
 HOMEDIR   = /c/msys64/home/BMcClennon/week3/P4600F21VISA
 
 # 3rd Party library directories to search
-LIBDIR    = /mingw64/lib $(HOMEDIR)/libs
+LIBDIR    = /mingw64/lib $(HOMEDIR)/lib
 LIBINCDIR = $(HOMEDIR)/includes
 
 # List sources, object files and libraries to be used
 SOURCES   = $(wildcard $(SRCDIR)/*.c )
 OBJECTS   = $(patsubst $(SRCDIR)/%.c, $(BUILDDIR)/%.o, $(SOURCES))
-LIBRARIES =
+LIBRARIES = visa64
 
 # Create command line arguments
 LIBCMD    = $(addprefix -l,$(LIBRARIES))
--- src/main.c
+++ src/main.c
@@ -4,18 +4,81 @@
 #include <math.h>
 #include "randdata.h"
 #include "calcfunctions.h"
+#include "visa.h"
 
-/*
-  TASK:
-
-  Create, in a separate file a set of functions to calculate:
-  The mean, RMS value and amplitude and smoothed version of the random data generated
-
-*/
 
 int main(int argc, char** argv)
 {
 
+  ViStatus status = VI_SUCCESS;
+  ViSession resource_manager;
+  ViSession scope_handle;
+  ViFindList resource_list;
+  long unsigned int num_inst;
+  char description[VI_FIND_BUFLEN];
+
+  status = viOpenDefaultRM(&resource_manager);//opens resource manager
+
+  if(status != VI_SUCCESS)
+  {
+    //do something about it
+    printf("Ooops");
+    exit(1);
+  }
+
+  else
+  {
+
+    status = viFindRsrc(resource_manager, "USB[0-9]::0x0699?*INSTR", &resource_list, &num_inst, description);
+
+    if(status != VI_SUCCESS)
+    {
+      printf("couldn't find any instruments");
+      fflush(stdout);
+      exit(1);
+    }
+
+    status = viOpen(resource_manager, description, VI_NULL, VI_NULL, &scope_handle);
+
+    if(status != VI_SUCCESS)
+    {
+      printf("couldn't connect to oscilloscope");
+      fflush(stdout);
+      exit(1);
+    }
+
+    printf("\nOpened scope");
+
+    char returned_message[128];
+    char scale_message[128];
+    char volt_message[128];
+
+    viPrintf(scope_handle, "*IDN?\n");
+    viScanf(scope_handle, "%t", returned_message);
+
+    printf(returned_message);
+    fflush(stdout);
+
+    viPrintf(scope_handle, "CH1:SCAle 0.7446\nHORizontal:SCAle 0.10\nCH1:VOLts?\n");
+    //viPrintf(scope_handle, "HORizontal:SCAle 1.0\n");
+
+    viScanf(scope_handle, "%t", volt_message);
+    printf("Voltage scale = %s\n", volt_message);
+    //fflush(stdout);
+
+
+    viPrintf(scope_handle, "CH1:SCAle?\n");
+    viScanf(scope_handle, "%t", scale_message);
+    printf("Vertical scale = %s\n", scale_message);
+    fflush(stdout);
+
+
+
+
+  }
+
+  return 0;
+  /*
   int ndata = 1024;
   int windowSize = 10;
   int smoothedLen = ndata - windowSize + 1;
@@ -53,4 +116,5 @@ int main(int argc, char** argv)
 
   fclose(outputfilesmoothed);
 
+  return 0;*/
 }
